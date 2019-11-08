76f28ed171b7bf0689fbeefef30aff6f0602262c
DoHuy <dohuy171@gmail.com>
2019-11-08 18:36:37 +0700

    init

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# VideoChat
+##To develop a real chat application
+### Technical Stack:
+1. Socket.io
+2. Express
+3. MongoDb
+4. Docker, Docker-swarm
+
---
+++ server.js
@@ -0,0 +1,22 @@
+import * as express from "express";
+import * as socketIO from "socket.io";
+
+const app = express();
+
+//set the template engine
+app.set('view engine', 'ejs');
+//set static file
+app.use(express.static('public'));
+
+// running
+
+const server = app.listen(process.env.port, ()=>{
+    console.log("App listenning at ", process.env.port);
+});
+//socket.io instantiation
+const io = require("socket.io")(server);
+
+io.on('connection', (socket)=>{
+    console.log("New user connection");
+});
+
