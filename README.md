c90b2a76cfbfdf98f974c2653f68f1ebbbbfc116
DoHuy <dohuy171@gmail.com>
2019-11-11 17:46:54 +0700

    add haproxy, build swarm for app

--- README.md
+++ README.md
@@ -3,5 +3,7 @@
 1. Socket.io
 2. Express
 3. MongoDb
-4. Docker, Docker-swarm
+4. Docker, Docker-Compose
+5. HAProxy
+6. Redis
 
--- server.js
+++ server.js
@@ -1,22 +1,25 @@
 import * as express from "express";
-import * as socketIO from "socket.io";
+// import * as socketIO from "socket.io";
 
 const app = express();
 
 //set the template engine
-app.set('view engine', 'ejs');
+// app.set('view engine', 'ejs');
 //set static file
-app.use(express.static('public'));
+// app.use(express.static('public'));
 
 // running
-
-const server = app.listen(process.env.port, ()=>{
-    console.log("App listenning at ", process.env.port);
+const server = app.listen(process.env.PORT, ()=>{
+    console.log("App listenning at ", process.env.PORT);
 });
 //socket.io instantiation
-const io = require("socket.io")(server);
+// const io = require("socket.io")(server);
 
-io.on('connection', (socket)=>{
-    console.log("New user connection");
+// io.on('connection', (socket)=>{
+//     console.log("New user connection");
+// });
+
+app.get("/", (req, res) => {
+    return "Hello world";
 });
 
